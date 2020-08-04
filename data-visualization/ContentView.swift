f408faec76da7dcb5a6a935a4c94a5f3155c0ed1
Ricardo <qq1043009478@icloud.com>
2020-08-04 08:47:00 +0800

    first commit

---
+++ data-visualization/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  data-visualization
+//
+//  Created by 骆顺旺 on 2020/8/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ data-visualization/data_visualizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  data_visualizationApp.swift
+//  data-visualization
+//
+//  Created by 骆顺旺 on 2020/8/4.
+//
+
+import SwiftUI
+
+@main
+struct data_visualizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
